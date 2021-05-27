d03ebe902b6f8e159a8c15e03e52c156c90d5f32
VoldemarGb <comendach90@gmail.com>
2021-05-27 14:04:53 +0300

    Update 5.js
    thinking

--- script5/5.js
+++ script5/5.js
@@ -1,1 +1,21 @@
-function mathOperation(arg1, arg2, operation)
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function adivision(a, b) {
+    return a / b;
+}
+
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+
+    }
+}
