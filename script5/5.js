4340e9685f8c02f2881884e2ed4ee3e27f6e37ba
VoldemarGb <comendach90@gmail.com>
2021-05-27 14:17:53 +0300

    с
    с

--- script5/5.js
+++ script5/5.js
@@ -10,7 +10,7 @@ function multiplication(a, b) {
     return a * b;
 }
 
-function adivision(a, b) {
+function division(a, b) {
     return a / b;
 }
 
@@ -18,11 +18,11 @@ function mathOperation(arg1, arg2, operation) {
     switch (operation) {
         case "+":
             return addition(arg1, arg2);
-        case "+":
-            return addition(arg1, arg2);
-        case "+":
-            return addition(arg1, arg2);
-        case "+":
-            return addition(arg1, arg2);
+        case "-":
+            return subtraction(arg1, arg2);
+        case "*":
+            return multiplication(arg1, arg2);
+        case "/":
+            return division(arg1, arg2);
     }
 }
