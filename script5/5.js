affa648278d689c7a48402843600a637cbd2ee3f
VoldemarGb <comendach90@gmail.com>
2021-06-01 23:27:23 +0300

    d

--- script5/5.js
+++ script5/5.js
@@ -1,28 +1,5 @@
-function addition(a, b) {
-    return a + b;
-}
+"use strict";
 
-function subtraction(a, b) {
-    return a - b;
-}
-
-function multiplication(a, b) {
-    return a * b;
-}
-
-function division(a, b) {
-    return a / b;
-}
-
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case "+":
-            return addition(arg1, arg2);
-        case "-":
-            return subtraction(arg1, arg2);
-        case "*":
-            return multiplication(arg1, arg2);
-        case "/":
-            return division(arg1, arg2);
-    }
-}
+for (let i = 0; i <= 9;)
+    console.log(i)
+{ }
