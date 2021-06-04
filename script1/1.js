464d4bcc3255aa273c3202195fa9ca2a448b4b88
VoldemarGb <comendach90@gmail.com>
2021-06-04 22:13:24 +0300

    attempt number 5

--- script1/1.js
+++ script1/1.js
@@ -4,6 +4,32 @@ function Product(name, price) {
     this.name = name;
     this.price = price;
 }
-this.show = function () {
-    alert(this.name + );
+
+Product.prototype.make25PercentDiscount = function () {
+    this.price = this.price - (this.price * 25 / 100);
 };
+
+const product1 = new Product('Товар 1', 1000);
+product1.make25PercentDiscount();
+
+console.log(product1);
+
+// Пробовал еще вот так. Как на уроке. 
+// 'use strict'
+//
+//function Product(name, price) {
+//   this.name = name;
+//   this.price = price;
+//   this.make25PercentDiscount = function (discount) {
+//      this.price = this price - (this.price/ 100*discount);
+//};
+//}
+//      count products = [
+//             new Product ('товар1', 100),
+//             new Product ('товар2', 200),
+//             new Product ('товар3', 300)
+//];
+//  for (let product of products) {
+//       product.make25PercentDiscount(25);
+//}
+// console.log (products);
